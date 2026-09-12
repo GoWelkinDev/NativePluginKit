1fe22b2fa05f5fc66d316756402e63a022622252
3cxc <81449257+3cxc@users.noreply.github.com>
2026-09-12 14:09:30 +0800

    feat: 示例客户端项目
    Signed-off-by: 3cxc <81449257+3cxc@users.noreply.github.com>

---
+++ UnknownSite0.Examples/Client/ExampleClient.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices;
+using UnknownSite0.Plugins.Loader;
+using UnknownSite0.Plugins.Loader.Constants;
+
+namespace ExampleClient
+{
+    public class ExampleClient
+    {
+        public static readonly PluginLoader PluginLoader = new PluginLoader(msg => Console.WriteLine(msg), HostApiBridge.CreateApiTable(), new Version(1, 0, 0));
+
+        public static void Main(String[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            PluginLoader.LoadPluginsFromDirectory(Path.Combine(Environment.CurrentDirectory, "plugins"));
+        }
+    }
+
+    public class HostApiBridge
+    {
+        // 构建 API 函数表
+        public static HostApiTable CreateApiTable()
+        {
+            HostApiBridgeBuilder.TransitionToSceneDelegate transition = (pathPtr, fadeOut) =>
+            {
+                string? scenePath = Marshal.PtrToStringUTF8(pathPtr);
+                if (string.IsNullOrEmpty(scenePath)) return;
+            };
+
+            HostApiBridgeBuilder.PrintLogDelegate print = (msgPtr) =>
+            {
+                string? msg = Marshal.PtrToStringUTF8(msgPtr);
+                if (!string.IsNullOrEmpty(msg)) Console.WriteLine(msg);
+            };
+
+            return HostApiBridgeBuilder.Create(transition, print);
+        }
+    }
+}
