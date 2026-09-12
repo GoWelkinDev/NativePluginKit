7c868d09185e48887a50fc413d20fa3a2e21b36f
3cxc <81449257+3cxc@users.noreply.github.com>
2026-09-12 14:35:42 +0800

    feat: 增加源生成器
    Signed-off-by: 3cxc <81449257+3cxc@users.noreply.github.com>

--- UnknownSite0.Examples/HelloWorldPlugin/HelloWorldPlugin.cs
+++ UnknownSite0.Examples/HelloWorldPlugin/HelloWorldPlugin.cs
@@ -1,5 +1,3 @@
-﻿using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using UnknownSite0.Plugins.Features.Logger;
 using UnknownSite0.Plugins.Loader.Plugins;
 
@@ -11,23 +9,12 @@ namespace HelloWorldPlugin
     /// </summary>
     public partial class HelloWorldPlugin : Plugin
     {
-        private static HelloWorldPlugin? _instance;
-
         public override string Name => "HelloWorldPlugin";
 
         public override string Description => "A simple hello world plugin for demonstration purposes.";
 
         public override string Author => "Your Name";
         public override Version RequiredApiVersion => new Version(1, 0, 0);
-
-#pragma warning disable CA2255
-        [ModuleInitializer]
-#pragma warning restore CA2255
-        public static void Init()
-        {
-            
-            SetPluginInfo(new HelloWorldPlugin());
-        }
 
         /// <summary>
         /// 插件启动时调用的方法，输出欢迎日志
@@ -37,25 +24,9 @@ namespace HelloWorldPlugin
             Log.PrintLog("HelloWorldPlugin started. Hello, World!");
         }
 
-        /// <summary>
-        /// 插件导出的初始化入口点，由插件加载器通过 <c>OnInit</c> 名称调用
-        /// </summary>
-        /// <param name="hostApiTablePtr">指向宿主 API 函数表的非托管指针</param>
-        /// <remarks>
-        /// 此方法被 <see cref="UnmanagedCallersOnlyAttribute"/> 标记，
-        /// 表示仅可由非托管代码调用。方法内部创建插件实例并调用
-        /// <see cref="Plugin.Initialize"/> 完成初始化。
-        /// </remarks>
-        [UnmanagedCallersOnly(EntryPoint = "OnInit")]
-        public static void OnInit(IntPtr hostApiTablePtr)
-        {
-            _instance = new HelloWorldPlugin();
-            Initialize(hostApiTablePtr, _instance);
-        }
-
         public override void OnStop()
         {
-            _instance = null;
+            Log.PrintLog("HelloWorldPlugin stopped. Goodbye!");
         }
     }
 }
